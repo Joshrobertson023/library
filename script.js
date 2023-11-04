c653327af07622bc04a2b2c7cfb3a52992ef4c55
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-11-04 14:59:00 -0500

    Form, list, and buttons working.
    Still need to add books to the library array when the form is submitted.

--- script.js
+++ script.js
@@ -13,11 +13,14 @@
 const listings = document.getElementById('listings'),
       show = document.getElementById('show'),
       btnAdd = document.getElementById('add'),
-      btnShow = document.getElementById('showList');
+      btnShow = document.getElementById('showList'),
+      dialog = document.querySelector('dialog'),
+      btnOK = document.querySelector('dialog button');
 
 /**********************************************************************/
 /*                          Global variables                          */
 /**********************************************************************/
+
 
 /**********************************************************************/
 /*                            Main progrma                            */
@@ -36,51 +39,83 @@ function Book(author, title, numPages, beenRead) {
    this.author = author;
    this.title = title;
    this.numPages = numPages;
-   this.beenRead = false;
+   this.beenRead = beenRead;
 }
 
-btnShow.addEventListener('click', () => {
-   displayBooks();
-});
-
 btnAdd.addEventListener('click', () => {
-   addBookToLibrary();
+   dialog.showModal();
+
+   let addBookForm = document.getElementById('addBookForm');
+
+   addBookForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let author = document.getElementById('inputAuthor');
+      let title = document.getElementById('inputTitle');
+      let number = document.getElementById('inputNumber');
+      let read = document.getElementById('inputRead');
+
+      if(author.value === "" || title.value === "" || number.value === "") {
+         alert("Make sure all fields are filled.");
+      } else {
+         addBookToLibrary(author.value, title.value, number.value, read.checked);
+         author.value = "";
+         title.value = "";
+         number.value = "";
+         read.checked = false;
+         dialog.close();
+      }
+   });
 });
 
-function addBookToLibrary() {
-   const listLine = document.createElement('li');
-   const btnRemove = document.createElement('button');
-   const btnRead = document.createElement('button');
-
-   listings.appendChild(listLine);
-   listLine.appendChild(btnRemove);
-   listLine.appendChild(btnRead);
-
-   btnRemove.textContent = "Delete";
-   btnRead.textContent = "Mark as read";
-
-   // Associate new book with library[]
-
-   // Add form
+function addBookToLibrary(author, title, number, read) {
+   let book = new Book(author, title, number, read);
+   console.log(book);
+   displayBooks();
    
-   btnRemove.addEventListener('click', () => {
-      listLine.remove;
-   });
-   btnRead.addEventListener('click', )
+   // Associate new book with library[]
 }
 
 function displayBooks() {
    library.forEach(function(book) {
       const showLine = document.createElement('li');
+      const divText = document.createElement('div');
       show.appendChild(showLine);
-      showLine.textContent = `Author: ${book.author}; 
-                              Title: ${book.title}; 
-                              Num of Pages: ${book.numPages}; `;
-      if(book.beenRead) {
-         showLine.textContent += `Has been read.`;
-      } else {
-         showLine.textContent += `Has not been read.`;
-      }
-      });
+
+      const btnRemove = document.createElement('button');
+      const btnRead = document.createElement('button');
+
+      showLine.appendChild(divText);
+      showLine.appendChild(btnRemove);
+      showLine.appendChild(btnRead);
+      
+      btnRemove.textContent = "Delete";
+      btnRead.textContent = "Mark as read";
+
+      updateDiv(divText, book.author, book.title, book.numPages, book.beenRead)
+
+      btnRemove.addEventListener('click', () => {
+         showLine.remove();
+      })
+      btnRead.addEventListener('click', () => {
+         if(book.beenRead) {
+            book.beenRead = false;
+         } else {
+            book.beenRead = true;
+         }
+         updateDiv(divText, book.author, book.title, book.numPages, book.beenRead);
+      })
+   });
 }
 
+function updateDiv(divText, author, title, pages, read) {
+   divText.textContent = '';
+   divText.textContent = `Author: ${author}; 
+   Title: ${title}; 
+   Num of Pages: ${pages}; `;
+   if(read) {
+      divText.textContent += `Has been read.`;
+   } else {
+      divText.textContent += `Has not been read.`;
+   }
+}
